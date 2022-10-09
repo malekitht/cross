ec801a3822e2e602074d52ef8eb3ee13464da206
Oleg Shkoloviy <shkoloviy392@gmail.com>
2022-10-09 21:13:14 +0300

    Solved task
    Soved first lab work. Test data worked as expected. Some "fool defend" has been added

---
+++ lab_1/Lab_1/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int x = 0;
+            int k = 0;
+            try
+            {
+                x = ReadNumbers()[0];
+                k = ReadNumbers()[1];
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("FILE IS INCORRECT OR NOT EXISTING! FOLLOW EXAMPLE BELOW!: \n55 10");
+                Console.WriteLine(ex.Message);
+            }
+            Double a = x/5;
+            Console.WriteLine(Factorial(a + k) / (Factorial(a) * Factorial(k)));
+
+        }
+
+        public static List <int> ReadNumbers()
+        {
+            using (StreamReader sr = new StreamReader("file.txt"))
+            {
+                List<int> a = new List<int>();
+                string numbers = sr.ReadToEnd();
+                foreach(var number in numbers.Split())
+                {
+                    a.Add(Convert.ToInt32(number));
+                }
+                if (a.Count() > 2)
+                {
+                    Exception ex = new Exception("FILE HAVE INCORRECT FORMAT");
+                    throw ex;
+                }
+                return a;
+            }
+        }
+
+        public static double Factorial(double a)
+        {
+            if (a % 1 == 0)
+            {
+                if (a == 0) return 1;
+                if (a > 0)
+                {
+                    int b = 1;
+                    for (int i = 1; i <= a; i++)
+                    {
+                        b *= i;
+                    }
+                    return b;
+                }
+            }
+            return double.NaN;
+        }
+    }
+}
